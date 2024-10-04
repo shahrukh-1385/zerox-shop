fa48591a2f235670c22d95b42e010ad41420dd62
shahrukh <116497129+shahrukh-1385@users.noreply.github.com>
2024-10-05 00:32:34 +0330

    Improve footer functionality

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,29 +10,44 @@ function Footer() {
     const body = encodeURIComponent(message);
     window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
   };
+  const handleLinkClick = (url: string) => {
+    window.open(url);
+  };
   return (
     <div className="bg-red-700 text-Slate-50 mt-4 h-80 flex flex-row-reverse justify-around items-center">
       <section className="mb-7 flex flex-row-reverse gap-32 mr-5">
         <div>
           <h3 className="font-vazirmatn400 border-b pb-2 mb-4 text-2xl">درباره ما</h3>
           <ul className="font-vazirmatn300 text-right">
-            <li className="mb-5 text-lg">چنل تلگرام</li>
-            <li className="mb-5 text-lg">اینستا گرام</li>
-            <li className="mb-5 text-lg">ایمیل</li>
+            <li className="mb-5 text-lg" onClick={() => handleLinkClick('https://t.me/#')}>
+              چنل تلگرام
+            </li>
+            <li className="mb-5 text-lg" onClick={() => handleLinkClick('https://www.instagram.com/#')}>
+              اینستا گرام
+            </li>
+            <li className="mb-5 text-lg" onClick={() => handleLinkClick('mailto:abolfql@gmail.com')}>
+              ایمیل
+            </li>
           </ul>
         </div>
         <div>
           <h3 className="font-vazirmatn400 border-b pb-2 mb-4 text-2xl">صفحات ما</h3>
           <ul className="font-vazirmatn300 text-right">
-            <li className="mb-5 text-lg">خانه</li>
-            <li className="mb-5 text-lg">فروشگاه</li>
-            <li className="mb-5 text-lg">سبد خرید</li>
+            <li className="mb-5 text-lg" onClick={() => handleLinkClick('/')}>
+              خانه
+            </li>
+            <li className="mb-5 text-lg" onClick={() => handleLinkClick('/Store')}>
+              فروشگاه
+            </li>
+            <li className="mb-5 text-lg" onClick={() => handleLinkClick('/shoppingPage')}>
+              سبد خرید
+            </li>
           </ul>
         </div>
       </section>
       <section>
         <div>
-          <input placeholder=". . . پیام های شما" type="text" className="placeholder:text-right placeholder:mr-3 placeholder:font-vazirmatn400  text-gray-900 text-right w-72 h-12 focus:outline-none border-none" value={message} onChange={(e:event)=>{ setMessage(e.target.value)}}/>
+          <input placeholder=". . . پیام های شما" type="text" className="placeholder:text-right placeholder:mr-3 placeholder:font-vazirmatn400  text-gray-900 text-right w-72 h-12 focus:outline-none border-none" value={message} onChange={(e: event) => { setMessage(e.target.value) }} />
           <button className="bg-Blue w-20 h-12" onClick={handleSendEmail}>ارسال</button>
         </div>
       </section>
--- src/pages/shopping/ShoppingPage.tsx
+++ src/pages/shopping/ShoppingPage.tsx
@@ -3,26 +3,34 @@ import PersianNumber from "../../components/persianNumber/PersianNumber";
 import ShoppingCartItem from "./shoppingCartItem/ShoppingCartItem";
 import { useShoppingCardContext } from "../../hooks/shoppingCardItemHooks/shoppingCardItemHooks";
 function ShoppingPage() {
-  const {cartItem } = useShoppingCardContext();
+  const { cartItem } = useShoppingCardContext();
 
   const cartItems = cartItem.map((item, index) => {
-    return(
-      <ShoppingCartItem key={index} {...item}/>
+    return (
+      <ShoppingCartItem key={index} {...item} />
     )
   })
   return (
     <Container>
-      <div className="">
-        {cartItems}
-      </div>
-      <div className="flex flex-col items-end mr-4">
-        <span>قیمت کالاها: {PersianNumber(1000)}</span>
-        <span>تخفیف: {PersianNumber(20)}</span>
-        <span>قیمت نهایی: {PersianNumber(8000)}</span>
-      </div>
-      <button className="bg-green text-Slate-50 mb-16 py-1.5 px-3.5 rounded-md font-vazirmatn400"> ثبت سفارش </button>
+      {
+        cartItem.length == 0
+          ? <div className="h-64 flex justify-center items-center"><span className="font-vazirmatn400 text-2xl">سبد خرید شما خالیه</span></div>
+          :
+          <>
+            <div className="">
+              {cartItems}
+            </div>
+            <div className="flex flex-col items-end mr-4">
+              <span>قیمت کالاها: {PersianNumber(1000)}</span>
+              <span>تخفیف: {PersianNumber(20)}</span>
+              <span>قیمت نهایی: {PersianNumber(8000)}</span>
+            </div>
+            <button className="bg-green text-Slate-50 mb-16 py-1.5 px-3.5 rounded-md font-vazirmatn400"> ثبت سفارش </button>
+          </>
+      }
     </Container>
-  )
+  );
+  
 }
 
 export default ShoppingPage;
